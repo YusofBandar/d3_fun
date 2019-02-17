07609677051a7550a168cdf16d3353397a4fefc6
unknown <ybandar1902@gmail.com>
2019-02-17 22:40:39 +0000

    Added Map Number Function

--- util.js
+++ util.js
@@ -5,3 +5,7 @@ function distance(x0, y0, x1, y1) {
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+function map (num, in_min, in_max, out_min, out_max){
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
