821940cddd3d926be3ebf5522fe2728db17a0873
unknown <ybandar1902@gmail.com>
2019-02-17 12:21:47 +0000

    Added Distance Calculation

--- particles.js
+++ particles.js
@@ -43,6 +43,12 @@ function renderParticles(amount = 500, options = {}) {
     });
 }
 
+const distance = (x0, y0, x1, y1) => Math.hypot(x1 - x0, y1 - y0);
+
+function distance(x0,y0,x1,y1){
+    return Math.hypot(x1 -x0,y1-y0);
+}
+
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
