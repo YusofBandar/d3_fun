3efa40ab1f5ca23e3e9e514130a6d73d74e8cf44
unknown <ybandar1902@gmail.com>
2019-02-17 12:02:54 +0000

    Drawing Random Moving Cirlces

--- particles.js
+++ particles.js
@@ -1,1 +1,41 @@
-console.log('hello world');
+function renderParticles() {
+    let w = 960,
+        h = 500;
+
+    let svg = d3.select("body").append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    let particles = svg.selectAll("circle")
+        .data(d3.range(1000).map(function () {
+            return {
+                x: w * Math.random(),
+                y: h * Math.random(),
+                dx: Math.random(),
+                dy: Math.random()
+            };
+        }))
+        .enter().append("circle")
+        .attr("r", 2.5);
+
+
+    d3.timer(function () {
+        // Update the circle positions.
+        particles
+            .attr("cx", function (d) {
+                d.x += d.dx;
+                if (d.x > w) d.x -= w;
+                else if (d.x < 0) d.x += w;
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                d.y += d.dy;
+                if (d.y > h) d.y -= h;
+                else if (d.y < 0) d.y += h;
+                return d.y;
+            });
+    });
+}
+
+
+renderParticles();
