754cb90b433386add54ebfa5a71f4deebbdbe853
Yusof Bandar <ybandar1902@gmail.com>
2019-02-19 16:41:33 +0000

    Getting Cx and Cy Of Each Orbit

--- circles/circles.js
+++ circles/circles.js
@@ -16,32 +16,64 @@ function renderCircles() {
         .attr("class", "orbits");
 
     //patterns
-    svg.append("g")
-        .attr("class","pattern");
+    let pattern = svg.append("g")
+        .attr("class", "pattern");
 
     //x axis
-    generateOrbit(orbits,amount,'xaxis',function(index){return ((index + 1) * 80) + 100},function(index){return 100});
+    generateOrbit(orbits, amount, 'xaxis', function (index) { return ((index + 1) * 80) + 100 }, function (index) { return 100 });
     //y axis
-    generateOrbit(orbits,amount,'yaxis',function(index){return 100},function(index){return ((index + 1) * 80) + 100});
+    generateOrbit(orbits, amount, 'yaxis', function (index) { return 100 }, function (index) { return ((index + 1) * 80) + 100 });
 
+
+    let orbitXData = orbits.selectAll(".xaxis .satellite").data()
+    let orbitYData = orbits.selectAll(".yaxis .satellite").data()
+    console.log(orbitXData);
+    console.log(orbitYData);
+
+
+    let cx = orbits.selectAll(".xaxis").select("#_0").select(".satellite").attr("cx");
+    console.log(cx);
+
+
+
+    for (let x = 0; x < amount; x++) {
+        for (let y = 0; y < amount; y++) {
+
+        }
+    }
+
+
+    for (let x = 0, length = orbitXData.length; x < length; x++) {
+
+        for (let y = 0, length = orbitYData.length; y < length; y++) {
+            let cx = orbits.selectAll(".xaxis").select(`#_${x}`).select(".satellite").attr("cx");
+            let cy = orbits.selectAll(".yaxis").select(`#_${y}`).select(".satellite").attr("cy");
+            pattern.append("circle")
+                .attr("r", 5)
+                .attr("cx", cx)
+                .attr("cy", cy);
+        }
+    }
 
     let t = 0;
     d3.timer(function () {
-        // Update the circle positions.
+        /* // Update the circle positions.
         orbits.selectAll(".satellite")
             .attr("transform", function (d) {
                 return `rotate(${t * d.rotationSpeed},${d.x},${d.y})`
-            })
+            }) */
 
-        t = t>360 ? 0 : t + 1;
+
+
+        t = t > 360 ? 0 : t + 1;
     });
 }
 
-function generateOrbit(node,amount=8,axisName,xfn,yfn) {
-   
-    
+function generateOrbit(node, amount = 8, axisName, xfn, yfn) {
+
+
     orbits = node.append("g")
-        .attr("class",axisName);
+        .attr("class", axisName);
 
     orbits = orbits.selectAll(".orbit")
         .data(d3.range(amount).map(function (index) {
@@ -50,7 +82,7 @@ function generateOrbit(node,amount=8,axisName,xfn,yfn) {
                 y: yfn(index),
                 rotationSpeed: index + 1,
                 orbitRadius: 30,
-                index
+                index: `_${index}`
             }
         }))
         .enter()
