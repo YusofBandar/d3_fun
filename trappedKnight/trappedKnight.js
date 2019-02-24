3225575df2e85dd31a65ad726735a480484c57d0
unknown <ybandar1902@gmail.com>
2019-02-24 23:35:59 +0000

    Generating Sequence

--- trappedKnight/trappedKnight.js
+++ trappedKnight/trappedKnight.js
@@ -1,4 +1,4 @@
-function spiralMatrix(size=10) {
+function spiralMatrix(size = 10) {
     let matrix = [];
 
     for (let i = 0; i < size; i++) {
@@ -9,7 +9,7 @@ function spiralMatrix(size=10) {
         matrix.push(row);
     }
 
-    let coords = [(size/2)-1, (size/2)-1];
+    let coords = [(size / 2) - 1, (size / 2) - 1];
 
     let xDistance = 1;
     let yDistance = 1;
@@ -51,10 +51,55 @@ function spiralMatrix(size=10) {
         }
     }
 
-    console.log(xDistance, yDistance);
-
-
+    console.log(matrix);
     return matrix;
 }
 
-console.log(spiralMatrix(30));
+function knightMovement(matrix) {
+    let knightMovements = [
+        [1, -2],
+        [-1, -2],
+        [2, -1],
+        [-2, -1],
+        [2, 1],
+        [-2, 1],
+        [1, 2],
+        [-1, 2]
+    ];
+
+    let knightPosition = [(matrix.length / 2) - 1, (matrix.length / 2) - 1];
+    matrix[(matrix.length / 2) - 1][(matrix.length / 2) - 1] = -1;
+
+    let possiable = true;
+    while (possiable) {
+        possiable = false;
+        let least = 99999999999;
+        let leastcoord = [];
+        
+        for (let i = 0, length = knightMovements.length; i < length; i++) {
+            let x = knightMovements[i][0] + knightPosition[0];
+            let y = knightMovements[i][1] + knightPosition[1];
+
+            if (y > -1 && y < (matrix.length - 1) && x > -1 && x < (matrix.length - 1)) {
+                let index = matrix[y][x];
+                if (index != -1 && index < least) {
+                    least = index;
+                    leastcoord.push(x);
+                    leastcoord.push(y);
+                    possiable = true;
+                }
+            }
+        }
+        
+        if(possiable){
+            matrix[leastcoord[1]][leastcoord[0]] = -1;
+            knightPosition[0] = leastcoord[0];
+            knightPosition[1] = leastcoord[1];
+            console.log(leastcoord[0], leastcoord[1], "at index ", least);
+            possiable = true;
+        }
+       
+    }
+}
+
+knightMovement(spiralMatrix(30));
