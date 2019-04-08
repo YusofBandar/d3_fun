02c1e1dc4d7f9af511f704389895ab26a1be2d08
Yusof Bandar <ybandar1902@gmail.com>
2019-04-08 13:07:59 +0100

    Animating donut chart

--- donuts/donuts.js
+++ donuts/donuts.js
@@ -1,38 +1,46 @@
 var dataset = [
-	{ name: 'IE', percent: 39.10 },
-	{ name: 'Chrome', percent: 32.51 },
-	{ name: 'Safari', percent: 13.68 },
-	{ name: 'Firefox', percent: 8.71 },
-	{ name: 'Others', percent: 6.01 }
+    { name: 'IE', percent: 39.10 },
+    { name: 'Chrome', percent: 32.51 },
+    { name: 'Safari', percent: 13.68 },
+    { name: 'Firefox', percent: 8.71 },
+    { name: 'Others', percent: 6.01 }
 ];
 
-var pie=d3.pie()
-        .value(function(d){return d.percent})
-        .sort(null)
-        .padAngle(.03);
- 
-var w=300,h=300;
- 
-var outerRadius=w/2;
-var innerRadius=100;
- 
+var pie = d3.pie()
+    .value(function (d) { return d.percent })
+    .sort(null)
+    .padAngle(.03);
 
- 
-var arc=d3.arc()
-        .outerRadius(outerRadius)
-        .innerRadius(innerRadius);
- 
-var svg=d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .append('g')
-        .attr("transform",'translate('+w/2+','+h/2+')')
-        
+var w = 300, h = 300;
 
-var path=svg.selectAll('path')
-        .data(pie(dataset))
-        .enter()
-        .append('path')
-        .attr("d",arc)
-  
+var outerRadius = w / 2;
+var innerRadius = 100;
+
+
+
+var arc = d3.arc()
+    .outerRadius(outerRadius)
+    .innerRadius(innerRadius);
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append('g')
+    .attr("transform", 'translate(' + w / 2 + ',' + h / 2 + ')')
+
+
+var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr("d", arc)
+
+path.transition()
+    .duration(1000)
+    .attrTween('d', function (d) {
+        var interpolate = d3.interpolate({ startAngle: 0, endAngle: 0 }, d);
+        return function (t) {
+            return arc(interpolate(t));
+        };
+    });
