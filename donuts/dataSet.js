9816d12763dcfafa01e17e23ecdeeaa8afe9742c
Yusof Bandar <ybandar1902@gmail.com>
2019-04-08 14:41:49 +0100

    Added real data
    only testing on apple's revenue for now

---
+++ donuts/dataSet.js
@@ -0,0 +1,30 @@
+var techCompanies = [
+    {
+        company: "apple",
+        colors: [],
+        data: [
+            {
+                label: "iphone",
+                amount: 166.7
+            },
+            {
+                label: "service",
+                amount: 37.2
+            },
+            {
+                label: "mac",
+                amount: 25.5
+            },
+            {
+                label: "ipad",
+                amount: 18.8
+            },
+            {
+                label : "other",
+                amount: 17.4
+            }
+        ]
+    }
+];
+
+
