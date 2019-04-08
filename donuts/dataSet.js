48498e79961aef4d9526f5ed8e6170781b3982fc
Yusof Bandar <ybandar1902@gmail.com>
2019-04-08 16:23:48 +0100

    Using whole dataset
    it looks good

--- donuts/dataSet.js
+++ donuts/dataSet.js
@@ -26,28 +26,110 @@ var techCompanies = [
         ]
     },
     {
-        company: "Apple",
-        colors: ["#010101","#535557","#636566","#939598","#bcbec0"],
+        company: "Amazon",
+        colors: ["#000000","#936016","#cb9440","#fea704","#ffc978","#fedaa2"],
         data: [
             {
-                label: "iphone",
-                amount: 166.7
+                label: "Online Stores",
+                amount: 123
             },
             {
-                label: "service",
-                amount: 37.2
+                label: "Third Party Sellers",
+                amount: 42.7
             },
             {
-                label: "mac",
-                amount: 25.5
+                label: "AWS",
+                amount: 25.7
             },
             {
-                label: "ipad",
-                amount: 18.8
+                label: "Physical Stores",
+                amount: 17.2
             },
             {
-                label : "other",
-                amount: 17.4
+                label : "Prime",
+                amount: 14.2
+            },
+            {
+                label: "Other",
+                amount: 10.1
+            }
+        ]
+    },
+    {
+        company: "Google",
+        colors: ["#fe0003","#ff3f2e","#fe7962","#a80002"],
+        data: [
+            {
+                label: "Properties Advertising",
+                amount: 96.3
+            },
+            {
+                label: "Network Advertising",
+                amount: 20.0
+            },
+            {
+                label: "Google Other",
+                amount: 20.0
+            },
+            {
+                label: "Other",
+                amount: 0.6
+            },
+        ]
+    },
+    {
+        company: "Facebook",
+        colors: ["#3f569e","#8787bb"],
+        data: [
+            {
+                label: "Facebook Ads",
+                amount: 55
+            },
+            {
+                label: "Payments and Other Fees",
+                amount: 0.8
+            },
+        ]
+    },
+    {
+        company: "Microsoft",
+        colors: ["#ffb500","#9dd230","#69c83f","#69c83f","#03b5c2","#00a4da","#468db5","#52758e","#4e5f6e"],
+        data: [
+            {
+                label: "Office",
+                amount: 28.3
+            },
+            {
+                label: "Azure",
+                amount: 26.1
+            },
+            {
+                label: "Windows",
+                amount: 19.5
+            },
+            {
+                label: "Gaming",
+                amount: 10.4
+            },
+            {
+                label: "Bing Ads",
+                amount: 7.0
+            },
+            {
+                label: "Enterprise Service",
+                amount: 5.8
+            },
+            {
+                label: "Devices",
+                amount: 5.1
+            },
+            {
+                label: "LinkedIn",
+                amount: 5.3
+            },
+            {
+                label: "Other",
+                amount: 2.8
             }
         ]
     }
